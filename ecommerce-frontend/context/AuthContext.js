f36f81bd093cd48954b92b6bf4220ab1f85e34d7
Caner Kuncu <warlordsw@gmail.com>
2021-05-06 08:42:17 +0300

    add createcontext, magic to the project

---
+++ ecommerce-frontend/context/AuthContext.js
@@ -0,0 +1,54 @@
+import { createContext, useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { Magic } from 'magic-sdk'
+import { MAGIC_PUBLIC_KEY } from '../utils/urls'
+
+const AuthContext = createContext()
+
+let magic
+
+export const AuthProvider = (props) => {
+  const [user, setUser] = useState(null)
+
+  const router = useRouter()
+
+  /**
+   * Adds email to user
+   * @param {string} email
+   */
+
+  const loginUser = async (email) => {
+    try {
+      await magic.auth.loginWithMagicLink({ email })
+      setUser({ email })
+      router.push('/')
+    } catch (err) {
+      console.log(err)
+      setUser(null)
+    }
+  }
+
+  /**
+   * Sets the user to null
+   */
+
+  const logoutUser = async () => {
+    try {
+      await magic.user.logout()
+      setUser(null)
+      router.push('/')
+    } catch (err) {}
+  }
+
+  useEffect(() => {
+    magic = new Magic(MAGIC_PUBLIC_KEY)
+  })
+
+  return (
+    <AuthContext.Provider value={{ user, loginUser, logoutUser }}>
+      {props.children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthContext
