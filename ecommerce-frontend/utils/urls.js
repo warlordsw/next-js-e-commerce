0bf7768477df07ca704d1a4c44bf25770149cb77
Caner Kuncu <warlordsw@gmail.com>
2021-05-06 08:43:26 +0300

    add utilities

---
+++ ecommerce-frontend/utils/urls.js
@@ -0,0 +1,23 @@
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337'
+
+export const MAGIC_PUBLIC_KEY =
+  process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY || 'pk_test_F4B2855543810606'
+
+/**
+ * Given an image return the Url
+ * Works for local and deployed strapis
+ * @param {any} image
+ */
+
+export const fromImageToUrl = (image) => {
+  if (!image) {
+    return '/vercel.svg'
+  }
+
+  if (image.url.indexOf('/') === 0) {
+    return `${API_URL}${image.url}`
+  }
+
+  return image.url
+}
