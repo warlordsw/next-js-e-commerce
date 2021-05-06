4cd9bc754499698f93b48e6d4b9b7e8d74770fbc
Caner Kuncu <warlordsw@gmail.com>
2021-05-06 11:21:58 +0300

    modify backend order.js

---
+++ ecommerce-backend/api/order/controllers/order.js
@@ -0,0 +1,46 @@
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  /**
+   * Only return orders that belongs to the logged in user
+   * @param {any} ctx
+   */
+
+  async find(ctx) {
+    const { user } = ctx.state; //This is the magic user
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.order.search({
+        ...ctx.query,
+        user: user.id,
+      });
+    } else {
+      entities = await strapi.services.order.find({
+        ...ctx.query,
+        user: user.id,
+      });
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.order })
+    );
+  },
+
+  /**
+   * Returns one order, as long as it belongs to the user
+   * @param {any} ctx
+   */
+
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+
+    const entity = await strapi.services.order.findOne({ id, user: user.id });
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+};
