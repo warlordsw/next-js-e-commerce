69191bb944cb520a6d18252ab489eb7ad1a77475
Caner Kuncu <warlordsw@gmail.com>
2021-05-06 11:24:29 +0300

    add styles to login.js

--- ecommerce-frontend/pages/login.js
+++ ecommerce-frontend/pages/login.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { useContext, useState } from 'react'
 import AuthContext from '../context/AuthContext'
+import styles from '../styles/Login.module.css'
 
 export default function Login() {
   const [email, setEmail] = useState('')
@@ -20,12 +21,15 @@ export default function Login() {
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
         <input
+          className={styles.input}
           type='email'
           value={email}
           onChange={(event) => setEmail(event.target.value)}
           placeholder='Email Address'
         />
-        <button type='submit'>Login</button>
+        <button className={styles.button} type='submit'>
+          Login
+        </button>
       </form>
     </div>
   )
