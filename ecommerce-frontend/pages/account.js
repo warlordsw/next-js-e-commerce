228bea57eaadfd4681cc66d4c8f1d23b5074aca7
Caner Kuncu <warlordsw@gmail.com>
2021-05-06 11:25:19 +0300

    add useOrders function to account.js

--- ecommerce-frontend/pages/account.js
+++ ecommerce-frontend/pages/account.js
@@ -1,10 +1,41 @@
 import Head from 'next/head'
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
+import { API_URL } from '../utils/urls'
 import Link from 'next/link'
 import AuthContext from '../context/AuthContext'
 
+const useOrders = (user, getToken) => {
+  const [orders, setOrders] = useState([])
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      if (user) {
+        try {
+          const token = await getToken()
+          const order_res = await fetch(`${API_URL}/orders`, {
+            headers: {
+              //prettier-ignore
+              'Authorization': `Bearer ${token}`,
+            },
+          })
+          const data = await order_res.json()
+          setOrders(data)
+        } catch (err) {
+          setOrders([])
+        }
+      }
+    }
+    fetchOrders()
+  }, [user])
+  return orders
+}
+
 export default function Account() {
-  const { user, logoutUser } = useContext(AuthContext)
+  const { user, logoutUser, getToken } = useContext(AuthContext)
+
+  const orders = useOrders(user, getToken)
+
+  console.log('Account.render orders', orders)
 
   if (!user) {
     return (
@@ -28,6 +59,7 @@ export default function Account() {
       </Head>
 
       <h2>Account page</h2>
+      <p>Logged in ass : {user.email}</p>
       <a href='#' onClick={logoutUser}>
         Logout
       </a>
